d61f648f3dbe9525c1862751cca2d09ed2556ec8
tmzzz <temir1201@gmail.com>
2020-04-11 22:30:01 +0300

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+import './Header.css';
+
+
+class Header extends React.Component {
+  render () {
+    return (
+      <div>
+        <header>
+          <div className="top-bar animate-dropdown"></div>
+          <div className="main-header">
+            <div className="container">
+              <h1 className="site-title">React Lite Level</h1>
+            </div>
+          </div>
+          <Nav />
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Header;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Nav.css';
+
+class Nav extends React.Component {
+  render () {
+    return (
+      <div>
+        <div className="header-nav">
+            <div className="container">
+                <nav>
+                    <ul>
+                        <li><a href="#">Главная</a></li>
+                        <li><a href="#">Пункты обмена</a></li>
+                        <li><a href="#">Контакты</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Nav;
